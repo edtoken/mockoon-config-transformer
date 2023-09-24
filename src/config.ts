9b3c3a666a0bdaac0eb332d9865d9072c1fa710f
ed.t <editied@gmail.com>
2023-09-24 20:12:41 +0200

    Fix version num

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const name = 'mockoon-config-transformer';
-export const version = '0.0.1-beta.40';
+export const version = '0.0.5';
 
 export const SUPPORTED_MIGRATION = 28;
 export const SUPPORTED_COMMANDS = {
